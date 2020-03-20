b2ecdbed7c38fa980392d2394a1860c7fb025a70
lukasz26671 <lukasz26671@wp.pl>
2020-03-20 22:50:49 +0100

    Update :D

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-const http = require('http');
+
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const youtubeSteam = require('youtube-audio-stream');
+
+const app = express();
 const port = process.env.PORT || 3000
 
+let config = {
+    mainEntry: "index.html"
+}
+
+// app.use(cors());
+
+// app.listen(port, ()=> {
+//     console.log(`Server running at ${port}`);
+// })
+// app.get('/', (req, res)=> {
+//     res.sendFile(`${__dirname}/index.html`);
+// })
+
+// app.get('/download', (req, res) => {
+//     var url = req.query.url;
+//     res.header('Content-Disposition', 'attachment; filename="video.mp3"');
+//     ytdl(url, {
+//         format: 'mp3'
+//     }).pipe(res);
+// });
+
+app.use(cors());
 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`<h1>Hello world!</h1><p>What are you doing here mate :)`)
-});
-
-server.listen(port, ()=> {
-    console.log(`Server running at port ${port}`);
+let httpServer = app.listen(port, ()=>{
+    console.log(`Server running on  ${httpServer.address().address}:${port}`)
 })
+
+app.get('/', (req, res)=>{
+    res.sendFile(`${__dirname}/${config.mainEntry}`)
+}) 
+
+app.get('/download', (req, res) => {
+    var url = req.query.URL;
+    var yid = req.query.ID;
+    res.header('Content-Disposition', 'attachment; filename="vid_converted.mp3"');
+
+    if(url != undefined){
+        ytdl(url, {
+            format: 'mp3'
+        }).pipe(res);
+    }
+    if(yid != undefined) {
+        ytdl(yid, {
+            format: 'mp3'
+        }).pipe(res);
+    }
+})
+
+
