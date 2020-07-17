4ba7b921d754ba4c89200f643504e754031acb81
lukasz26671 <lukasz26671@wp.pl>
2020-07-17 11:22:31 +0200

    kkkkkkkkkk

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ const port = process.env.PORT || 3000
     
             console.log(`GET: ${req.url}`);
     
-            let url = `https://youtube.com/watch?v=${req.url.split('/')[3]}`;
+            let url = `https://youtube.com/watch?v=${req.url.split('/')[2]}`;
             
     
             res.set({"Content-Type": "audio/mpeg" });
