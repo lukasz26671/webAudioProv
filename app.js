d654a175122860c80023e1c4b82b97bc9421f958
lukasz26671 <lukasz26671@wp.pl>
2020-07-19 21:47:06 +0200

    i'm trying to fix it :(

--- app.js
+++ app.js
@@ -1,110 +1,102 @@
+
 const express = require('express');
+const fs = require('fs');
 const cors = require('cors');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const app = express();
 const port = process.env.PORT || 3000
 
-// if(process.argv[2] == 'check') {
-//     if(process.argv[3] != null) {
-//         (async () => {
-//             const {ytdl, ffmpeg, ffmpegPath} = await importDependencies();
-
-//             try {
-//                 if(req.url.includes("favicon")) return;
-//                 ffmpeg().kill();
-        
-//                 var stream = ffmpeg().setFfmpegPath(ffmpegPath);
-        
-//                 stream.on('error', (err, stdout, stderr)=>{
-//                     console.log(err.message);
-//                 })
-        
-//                 console.log(`GET: ${req.url}`);
-        
-//                 let url = `https://youtube.com/watch?v=${req.url.split('/')[2]}`;
-        
-//                 res.set({"Content-Type": "audio/mpeg" });
-        
-//                 res.statusCode = 302;
-        
-//                 stream.input(ytdl(url)).toFormat('mp3').on('end', ()=>{
-//                     console.log('Finished');
-//                 });
-//                 process.exit(0)
-//             } catch (error) {
-//                 process.exit(1)
-//             }
-
-//         })();
-//     }
-// } else {
-    let config = {
-        mainEntry: "index.html"
-    }
-    
-    app.use(cors());
-    
-    let httpServer = app.listen(port, ()=>{
-        console.log(`Server running on  ${httpServer.address().address}:${port}`)
-    })
-    
-    
-    
-    app.get('/', (req, res)=>{
-        res.sendStatus(200);
-    }) 
-    
-    app.get('/api/***********', (req, res) => {
-        const {ytdl, ffmpeg, ffmpegPath} = importDependencies();
-    
-        try {
-            if(req.url.includes("favicon")) return;
-            ffmpeg().kill();
-    
-            var stream = ffmpeg().setFfmpegPath(ffmpegPath);
-    
-            stream.on('error', (err, stdout, stderr)=>{
-                console.log(err);
-            })
-    
-            console.log(`GET: ${req.url}`);
-    
-            let u = req.url.split('/api/');
-            let url = `https://youtube.com/watch?v=${u[1]}`;
-            
-    
-            res.set({"Content-Type": "audio/mpeg" });
-    
-            res.statusCode = 302;
-    
-            stream.input(
-                    ytdl(url)).toFormat('mp3').pipe(res, {end: true}
-            ).on('end', ()=>{
-                console.log('Finished');
-            });
-    
-        } catch (error) {
-            res.statusMessage = "API error"
-            console.log(error)
-            res.status(500).end();
-        }
-    })
-    
-    /*
-       ! Używać poprzez app.use() na końcu kodu
-       TODO: Bardziej przejrzysty error message
-    */
-    function NotFound(req, res, next) {
-        res.status(404).send('404: Not found')
-    }
-    
-    app.use(NotFound);
-    
-// }
-function importDependencies() {
-
-    const ytdl = require('ytdl-core');
-    const ffmpeg =  require('fluent-ffmpeg');
-    const ffmpegPath =  require('@ffmpeg-installer/ffmpeg').path;
-
-    return {ytdl, ffmpeg, ffmpegPath};
+let config = {
+    mainEntry: "index.html"
 }
+
+app.use(cors());
+
+let httpServer = app.listen(port, ()=>{
+    console.log(`Server running on  ${httpServer.address().address}:${port}`)
+})
+
+
+app.get('/', (req, res)=>{
+    res.sendStatus(200);
+}) 
+
+app.get('/download', (req, res) => {
+    try {
+        var url = req.query.URL;
+        var yid = req.query.ID;
+        res.header('Content-Disposition', 'attachment; filename="vid_converted.mp4"');
+
+        if(url != undefined){
+            ytdl(url, {
+                format: 'mp4',
+                quality: "highestvideo"
+            }).pipe(res);
+            res.statusCode = 302;
+        }
+    } catch(error) {
+        res.statusMessage = "API error"
+        res.statusCode = 500;
+        console.log(error)
+        res.end();
+    }
+})
+
+app.get(/*/^(?:[-a-zA-Z_0-9\/]){0,10}(?:\w+)$/g*/"/***********", (req, res) => {
+    try {
+        if(req.url.includes("favicon")) return;
+        ffmpeg().kill();
+
+        var stream = ffmpeg().setFfmpegPath(ffmpegPath);
+
+        stream.on('error', (err, stdout, stderr)=>{
+            console.log(err.message);
+        })
+        console.log(req.url)
+        let u = req.url.split('/')[1];
+        let ur = `https://youtube.com/watch?v=${u}`;
+
+        res.set({"Content-Type": "audio/mpeg" });
+
+        res.statusCode = 302;
+
+        stream.input(
+                ytdl(ur)).toFormat('mp3').pipe(res, {end: true}
+        ).on('end', ()=>{
+            console.log('Finished');
+        });
+
+    } catch (error) {
+        res.statusMessage = "API error"
+        console.log(error)
+        res.status(500).end();
+    }
+})
+
+function conv(form, {url="", yid=undefined}, pipe) {
+    if(url != ""){
+        ytdl(url, {
+            format: form
+        }).pipe(pipe);
+    }
+}
+
+// app.use(cors());
+
+// app.listen(port, ()=> {
+//     console.log(`Server running at ${port}`);
+// })
+// app.get('/', (req, res)=> {
+//     res.sendFile(`${__dirname}/index.html`);
+// })
+
+// app.get('/download', (req, res) => {
+//     var url = req.query.url;
+//     res.header('Content-Disposition', 'attachment; filename="video.mp3"');
+//     ytdl(url, {
+//         format: 'mp3'
+//     }).pipe(res);
+// });
+
