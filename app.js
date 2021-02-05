463d6e45e5a027ab36aacfa0ebe621ed8a86eb99
lukasz26671 <lukasz26671@wp.pl>
2021-02-05 09:59:28 +0100

    updated error page, cleanup

--- app.js
+++ app.js
@@ -8,7 +8,29 @@ const fetch = require('node-fetch')
 const port = process.env.PORT || 80
 
 // "node-fetch": "^2.6.1",
-
+const encoding_headers = {
+    mp3:{
+        'Accept-Ranges': 'bytes',
+        'Connection':'keep-alive',
+        'Transfer-Encoding':'chunked',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        "Content-Disposition":"inline",
+        "Content-Transfer-Enconding":"binary",
+        'Content-Type': 'audio/mpeg'
+    }, 
+    mp4:{
+        'Accept-Ranges': 'bytes',
+        'Connection':'keep-alive',
+        'Transfer-Encoding':'chunked',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        "Content-Disposition":"inline",
+        "Content-Transfer-Enconding":"binary",
+        'Content-Type': 'video/mp4',
+        'Success':'true'
+    }
+}
 
 
 //todo move to separate file
@@ -26,13 +48,7 @@ let httpServer = app.listen(port, ()=>{
 //? Status for API and website
 app.get('/api/status', async (req, res) => {
 
-    let websiteStatus = (await fetch(`https://website-audioprovider.herokuapp.com/`)).ok;
-    let apiStatus = (await fetch(`https://website-audioprovider.herokuapp.com/download?ID=kJQP7kiw5Fk&TYPE=mp3`)).ok;
-
-    const response = {
-        website: websiteStatus ? `Available` : `Unavaliable`,
-        api: apiStatus ? `Available` : `Unavailable`,
-    }
+    const response = CheckStatus();
 
     await res.json({
         code: 200,
@@ -43,13 +59,7 @@ app.get('/api/status', async (req, res) => {
 //? Status for API and website with visualization
 app.get('/status', async (req, res) => {
 
-    let websiteStatus = (await fetch(`https://website-audioprovider.herokuapp.com/`)).ok;
-    let apiStatus = (await fetch(`https://website-audioprovider.herokuapp.com/download?ID=kJQP7kiw5Fk&TYPE=mp3`)).ok;
-
-    const response = {
-        website: websiteStatus ? `Available` : `Unavaliable`,
-        api: apiStatus ? `Available` : `Unavailable`,
-    }
+    const response = CheckStatus();
 
     let generatedPage = `
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"><style>*{font-family: 'Roboto'}</style>
@@ -74,32 +84,7 @@ app.get('/download', async (req, res) => {
         var url = req.query.URL;
         var yid = req.query.ID;
         var tp = req.query.TYPE
-        
-        const resHeaders = {
-            mp3: {
-                'Accept-Ranges': 'bytes',
-                'Connection':'keep-alive',
-                'Transfer-Encoding':'chunked',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': '*',
-                "Content-Disposition":"inline",
-                "Content-Transfer-Enconding":"binary",
-                'Content-Type': 'audio/mpeg'
-            },
-            mp4: {
-                'Accept-Ranges': 'bytes',
-                'Connection':'keep-alive',
-                'Transfer-Encoding':'chunked',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': '*',
-                "Content-Disposition":"inline",
-                "Content-Transfer-Enconding":"binary",
-                'Content-Type': 'video/mp4',
-                'Success':'true'
-            }
-        }
-
-        
+     
         if(tp === undefined) tp = 'mp4'
         
         const info = {
@@ -118,7 +103,7 @@ app.get('/download', async (req, res) => {
                 stream.pipe(res)
 
                 res.writeHead(200, 
-                    (info.fileType === 'mp4') ? resHeaders.mp4 : resHeaders.mp3
+                    (info.fileType === 'mp4') ? encoding_headers.mp4 : encoding_headers.mp3
                 );
             });
 
@@ -157,7 +142,28 @@ function downloadFromYoutube(url, format='mp4') {
 //! Używać poprzez app.use() na końcu kodu
 //TODO: Bardziej przejrzysty error message
 function NotFound(req, res, next) {
-    res.status(404).send('404: Not found')
+    res.status(404).send(
+        `
+        ${ErrorCodeStyle()}
+        <h1>404</h1>
+        <p>The page you requested does not exist.</p>
+        `
+    )
+}
+
+const CheckStatus = async ()=> {
+    let websiteStatus = (await fetch(`https://website-audioprovider.herokuapp.com/`)).ok;
+    let apiStatus = (await fetch(`https://website-audioprovider.herokuapp.com/download?ID=kJQP7kiw5Fk&TYPE=mp3`)).ok;
+
+    const response = {
+        website: websiteStatus ? `Available` : `Unavaliable`,
+        api: apiStatus ? `Available` : `Unavailable`,
+    }
+    return response;
+}
+
+const ErrorCodeStyle = () => {  
+    return `<style>@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap'); @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap'); h1 { font-family: 'Roboto'; } p { font-family: 'Open Sans'}</style>`
 }
 
 app.use(NotFound);
